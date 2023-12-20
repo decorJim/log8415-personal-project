a83eaa46105e3c0c8ca5ca2b1f019f0d441edc04
Victor Kim <victorkim@hotmail.ca>
2023-12-20 18:40:49 -0500

    trying old configuration with single shell

--- master_setup.sh
+++ master_setup.sh
@@ -9,56 +9,54 @@ IP_ADDRESS_2=$2
 IP_ADDRESS_3=$3
 IP_ADDRESS_4=$4
 
-echo "IP1: $IP_ADDRESS_1, IP2: $IP_ADDRESS_2, IP3: $IP_ADDRESS_3, IP4 $IP_ADDRESS_4" > clusterip.txt
-sudo apt update -y
-sudo apt-get install -y sysbench unzip expect
-sudo mkdir -p /opt/mysqlcluster/home
-sudo wget -O /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
-sudo tar xzf /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz --directory=/opt/mysqlcluster/home
-sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 /opt/mysqlcluster/home/mysqlc
-echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profile.d/mysqlc.sh
-source /etc/profile.d/mysqlc.sh
-echo "export PATH=$MYSQLC_HOME/bin:$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
-source /etc/profile.d/mysqlc.sh
-sudo apt-get update && sudo apt-get -y install libncurses5
-sudo mkdir -p /opt/mysqlcluster/deploy
-sudo mkdir -p /opt/mysqlcluster/deploy/conf
-sudo mkdir -p /opt/mysqlcluster/deploy/mysqld_data
-sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
-     
-sudo touch /opt/mysqlcluster/deploy/conf/my.cnf
-echo "[mysqld]" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "ndbcluster" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "datadir=/opt/mysqlcluster/deploy/mysqld_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "basedir=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "port=3306" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-     
-sudo touch /opt/mysqlcluster/deploy/conf/config.ini
-echo "[ndb_mgmd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_1//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=1" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "[ndbd default]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "noofreplicas=3" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-     
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_2//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=2" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_3//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=3" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_4//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=4" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-
-echo "[mysqld]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=50" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-
-cd /opt/mysqlcluster/home/mysqlc
-sudo scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data
-sudo chown -R root:root /opt/mysqlcluster/home/mysqlc
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/
-ndb_mgm -e show
+echo "IP1: $IP_ADDRESS_1, IP2: $IP_ADDRESS_2, IP3: $IP_ADDRESS_3, IP4 $IP_ADDRESS_4" | sudo tee -a clusterip.txt
+sudo apt-get install libaio1 -y
+sudo apt-get install unzip -y
+sudo apt-get install libmecab2 -y
+sudo apt-get install sysbench -y
+sudo apt-get install libncurses5 -y
+sudo apt-get install libtinfo5 -y
+sudo wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster-community-management-server_7.6.6-1ubuntu18.04_amd64.deb
+sudo dpkg -i mysql-cluster-community-management-server_7.6.6-1ubuntu18.04_amd64.deb
+sudo mkdir -p /var/lib/mysql-cluster
+sudo touch /var/lib/mysql-cluster/config.ini
+echo "[ndbd default]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "NoOfReplicas=3" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "[ndb_mgmd]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "hostname=ip-${IP_ADDRESS_1//./-}.ec2.internal" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "NodeId=1" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "datadir=/var/lib/mysql-cluster" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "[ndbd]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "hostname=ip-${IP_ADDRESS_2//./-}.ec2.internal" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "NodeId=2" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "datadir=/usr/local/mysql/data" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "[ndbd]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "hostname=ip-${IP_ADDRESS_3//./-}.ec2.internal" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "NodeId=3" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "datadir=/usr/local/mysql/data" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "[ndbd]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "hostname=ip-${IP_ADDRESS_4//./-}.ec2.internal" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "NodeId=4" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "datadir=/usr/local/mysql/data" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "[mysqld]" | sudo tee -a /var/lib/mysql-cluster/config.ini
+echo "hostname=ip-${IP_ADDRESS_1//./-}.ec2.internal" | sudo tee -a /var/lib/mysql-cluster/config.ini
+source /var/lib/mysql-cluster/config.ini
+sudo mkdir -p /etc/systemd/system/
+sudo touch /etc/systemd/system/ndb_mgmd.service
+echo "[Unit]" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "Description=NDB Cluster Management Server" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "After=network.target auditd.service" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "[Service]" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "Type=forking" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "ExecStart=/usr/sbin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "ExecReload=/bin/kill -HUP $MAINPID" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "KillMode=process" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "Restart=on-failure" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "[Install]" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/ndb_mgmd.service
+source /etc/systemd/system/ndb_mgmd.service
+sudo systemctl daemon-reload
+sudo systemctl enable ndb_mgmd
+sudo systemctl start ndb_mgmd
+sudo touch status.ini
+sudo systemctl status ndb_mgmd >> status.ini
