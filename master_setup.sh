731a80acac8559c237aa70e634be425552fd1eb2
Victor Kim <victorkim@hotmail.ca>
2023-12-21 15:57:00 -0500

    try modifying master_setup

--- master_setup.sh
+++ master_setup.sh
@@ -10,60 +10,62 @@ IP_ADDRESS_3=$3
 IP_ADDRESS_4=$4
 
 echo "IP1: $IP_ADDRESS_1, IP2: $IP_ADDRESS_2, IP3: $IP_ADDRESS_3, IP4 $IP_ADDRESS_4" > clusterip.txt
+
 sudo apt update -y
-sudo apt-get install -y sysbench unzip expect
+sudo apt-get install -y sysbench unzip expect libncurses5
+
 sudo mkdir -p /opt/mysqlcluster/home
-sudo wget -O /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
-sudo tar xzf /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz --directory=/opt/mysqlcluster/home
-sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 /opt/mysqlcluster/home/mysqlc
+cd /opt/mysqlcluster/home
+sudo wget -O mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
+sudo tar xzf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
+sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
 echo "export PATH=$MYSQLC_HOME/bin:$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
-sudo apt-get update && sudo apt-get -y install libncurses5
-sudo mkdir -p /opt/mysqlcluster/deploy
-sudo mkdir -p /opt/mysqlcluster/deploy/conf
-sudo mkdir -p /opt/mysqlcluster/deploy/mysqld_data
-sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
-     
-sudo touch /opt/mysqlcluster/deploy/conf/my.cnf
-echo "[mysqld]" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "ndbcluster" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "datadir=/opt/mysqlcluster/deploy/mysqld_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "basedir=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-echo "port=3306" | sudo tee -a /opt/mysqlcluster/deploy/conf/my.cnf
-     
-sudo touch /opt/mysqlcluster/deploy/conf/config.ini
-echo "[ndb_mgmd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_1//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=1" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "[ndbd default]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "noofreplicas=3" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-     
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_2//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=2" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
 
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_3//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=3" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
+# Create directories for MySQL Cluster deployment
+sudo mkdir -p /opt/mysqlcluster/deploy /opt/mysqlcluster/deploy/conf /opt/mysqlcluster/deploy/mysqld_data /opt/mysqlcluster/deploy/ndb_data
 
-echo "[ndbd]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "hostname=ip-${IP_ADDRESS_4//./-}.ec2.internal" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=4" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
+# Create and configure the my.cnf file
+sudo cp /etc/mysql/my.cnf /etc/mysql/my.cnf.backup
+sudo tee /etc/mysql/my.cnf > /dev/null <<EOF
+[mysqld]
+ndbcluster
+datadir=/opt/mysqlcluster/deploy/mysqld_data
+basedir=/opt/mysqlcluster/home/mysqlc
+port=3306
+EOF
 
-echo "[mysqld]" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-echo "nodeid=50" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
+# Create and configure the config.ini file
+sudo tee /opt/mysqlcluster/deploy/conf/config.ini > /dev/null <<EOF
+[ndb_mgmd]
+hostname=${IP_ADDRESS_1}
+datadir=/opt/mysqlcluster/deploy/ndb_data
+nodeid=1
+[ndbd default]
+noofreplicas=3
+datadir=/opt/mysqlcluster/deploy/ndb_data
+[ndbd]
+hostname=${IP_ADDRESS_2}
+nodeid=2
+[ndbd]
+hostname=${IP_ADDRESS_3}
+nodeid=3
+[ndbd]
+hostname=${IP_ADDRESS_4}
+nodeid=4
+[mysqld]
+nodeid=50
+EOF
+
+sudo ufw allow from $IP_ADDRESS_2
+sudo ufw allow from $IP_ADDRESS_3
+sudo ufw allow from $IP_ADDRESS_4
+sudo ufw allow 3306
+sudo ufw allow 1186
 
 sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/
 
 ndb_mgm -e show
-
-ufw allow from $IP_ADDRESS_2
-ufw allow from $IP_ADDRESS_3
-ufw allow from $IP_ADDRESS_4
-
-
 
