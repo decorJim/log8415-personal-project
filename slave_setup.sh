c7e91c2e5753cb024985830e4f1261119327b763
Victor Kim <victorkim@hotmail.ca>
2023-12-21 15:40:17 -0500

    change slave node setup

--- slave_setup.sh
+++ slave_setup.sh
@@ -6,11 +6,12 @@ fi
 
 IP_ADDRESS=$1
 
+
 sudo apt-get update -y
-sudo apt-get install -y sysbench
+sudo apt-get install -y sysbench libncurses5
+
 sudo mkdir -p /opt/mysqlcluster/home
 cd /opt/mysqlcluster/home
-
 sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
@@ -18,8 +19,22 @@ echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profi
 source /etc/profile.d/mysqlc.sh
 echo "export PATH=$MYSQLC_HOME/bin:$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
-sudo apt-get update && sudo apt-get -y install libncurses5
+
+
 sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
 sudo chmod -R 777 /opt/mysqlcluster/deploy/ndb_data/
+
+
 ndbd -c "ip-${IP_ADDRESS//./-}.ec2.internal:1186"
+
+MYSQL_CNF="/etc/mysql/conf.d/cluster.cnf"
+sudo touch $MYSQL_CNF
+echo "[mysqld]" | sudo tee -a $MYSQL_CNF
+echo "ndbcluster" | sudo tee -a $MYSQL_CNF
+echo "datadir=/opt/mysqlcluster/deploy/mysqld_data" | sudo tee -a $MYSQL_CNF
+echo "basedir=/opt/mysqlcluster/home/mysqlc" | sudo tee -a $MYSQL_CNF
+echo "port=3306" | sudo tee -a $MYSQL_CNF
+
+sudo systemctl restart mysql || sudo service mysql restart
+
 ndb_mgm -e show
